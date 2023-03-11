d8ab9632ab8651af4718c7daccf9748b29625a3e
Eamonn Hegarty <eamonnhegartyy@hotmail.co.uk>
2023-03-11 19:58:33 +0000

    changed job spec

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,10 +1,10 @@
 describe("Home Page tests - dev", () => {
   it("Correctly visits the Home page", () => {
-    cy.visit("http://localhost:3000");
+    cy.visit("https://aswa-tccc-bib-dev-pyth-01.azurewebsites.net/");
     cy.location("pathname").should("eq", "/");
   });
   it("Correctly visits the About page", () => {
-    cy.visit("http://localhost:3000/about");
+    cy.visit("https://aswa-tccc-bib-dev-pyth-01.azurewebsites.net/about");
     cy.location("pathname").should("eq", "/about");
   });
 });
