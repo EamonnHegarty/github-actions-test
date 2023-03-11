a4087929be83b00321a8ed05dcb67d878714b46d
Eamonn Hegarty <eamonnhegartyy@hotmail.co.uk>
2023-03-11 20:18:00 +0000

    changed yml

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,10 +1,10 @@
 describe("Home Page tests - dev", () => {
   it("Correctly visits the Home page", () => {
-    cy.visit("https://aswa-tccc-bib-dev-pyth-01.azurewebsites.net/");
+    cy.visit("http://localhost:3000");
     cy.location("pathname").should("eq", "/");
   });
   it("Correctly visits the About page", () => {
-    cy.visit("https://aswa-tccc-bib-dev-pyth-01.azurewebsites.net/about");
+    cy.visit("http://localhost:3000/about");
     cy.location("pathname").should("eq", "/about");
   });
 });
