40948d8d02703fac71f5cb28a8319f38677997a8
Eamonn Hegarty <eamonnhegartyy@hotmail.co.uk>
2023-03-13 20:48:37 +0000

    CI

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,17 +1,16 @@
 describe("Home Page tests", () => {
-  let url = "http://localhost:3000";
   it("Correctly visits the Home page", () => {
-    cy.visit(url);
+    cy.visit(Cypress.env("BASE_URL"));
     cy.location("pathname").should("eq", "/");
   });
 
   it("Correctly visits the EMEA BIP page", () => {
-    cy.visit(`${url}/emea`);
+    cy.visit(`${Cypress.env("BASE_URL")}/emea`);
     cy.location("pathname").should("eq", "/emea");
   });
 
   it("Correctly visits the About Us page", () => {
-    cy.visit(`${url}/about`);
+    cy.visit(`${Cypress.env("BASE_URL")}/about`);
     cy.location("pathname").should("eq", "/about");
   });
 });
