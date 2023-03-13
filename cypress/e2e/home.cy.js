6e1f1774d7d6082e0b4437981287f9472093b3bb
Eamonn Hegarty <eamonnhegartyy@hotmail.co.uk>
2023-03-13 13:47:15 +0000

    CI

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,10 +1,17 @@
-import { getUrlForEnvironment } from "../support/utils";
-
-describe("Home Page tests - dev", () => {
-  const url = getUrlForEnvironment(process.env.APP_ENV || "dev");
-  it(`Correctly visits the Home page:  ${url}`, () => {
+describe("Home Page tests", () => {
+  let url = "http://localhost:3000";
+  it("Correctly visits the Home page", () => {
     cy.visit(url);
-    console.log(url);
     cy.location("pathname").should("eq", "/");
+  });
+
+  it("Correctly visits the EMEA BIP page", () => {
+    cy.visit(`${url}/emea`);
+    cy.location("pathname").should("eq", "/emea");
+  });
+
+  it("Correctly visits the About Us page", () => {
+    cy.visit(`${url}/about`);
+    cy.location("pathname").should("eq", "/about");
   });
 });
