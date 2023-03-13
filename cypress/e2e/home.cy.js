d24b17f2c3ed8c1b8a842e339414c1fdab742d3a
Eamonn Hegarty <eamonnhegartyy@hotmail.co.uk>
2023-03-13 13:04:41 +0000

    CI

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,8 +1,8 @@
 import { getUrlForEnvironment } from "../support/utils";
 
 describe("Home Page tests - dev", () => {
-  it("Correctly visits the Home page", () => {
-    const url = getUrlForEnvironment(process.env.APP_ENV || "dev");
+  const url = getUrlForEnvironment(process.env.APP_ENV || "dev");
+  it(`Correctly visits the Home page:  ${url}`, () => {
     cy.visit(url);
     console.log(url);
     cy.location("pathname").should("eq", "/");
