cb3476caa7d6fb732a18c9f9a8fc2075fc6e997d
Eamonn Hegarty <eamonnhegartyy@hotmail.co.uk>
2023-03-14 08:05:02 +0000

    CI

--- cypress.config.js
+++ cypress.config.js
@@ -1,20 +1,13 @@
 const { defineConfig } = require("cypress");
-// const env = require("./cypress.env.json");
-
-// const isLocal = env.ENVIRONMENT === "local";
 
 module.exports = defineConfig({
-  e2e: {
-    baseUrl: "http://localhost:3000",
-    setupNodeEvents(on, config) {
-      require("@cypress/code-coverage/task")(on, config);
-      // include any other plugin code...
+  baseUrl: null,
+  setupNodeEvents(on, config) {
+    require("@cypress/code-coverage/task")(on, config);
+    // include any other plugin code...
 
-      // It's IMPORTANT to return the config object
-      // with any changed environment variables
-      return config;
-    },
-    // video: isLocal,
-    // screenshotsFolder: isLocal ? "cypress/screenshots" : false,
+    // It's IMPORTANT to return the config object
+    // with any changed environment variables
+    return config;
   },
 });
--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,6 +1,6 @@
 describe("My test suite", () => {
   beforeEach(() => {
-    cy.visit("/");
+    cy.visit(Cypress.env("APP_URL"));
   });
   it("My test", () => {
     cy.location("pathname").should("eq", "/");
